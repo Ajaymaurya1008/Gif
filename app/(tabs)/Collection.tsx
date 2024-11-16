acc2ef622d58f5c9384da03f901f77b8881a76b3
Ajaymaurya1008 <ajaykvmaurya@gmail.com>
2024-11-17 02:40:17 +0530

    chore : update name

--- app/(tabs)/Collection.tsx
+++ app/(tabs)/Collection.tsx
@@ -1,9 +1,9 @@
 import { View, Text } from "react-native";
 
-export default function HomeScreen2() {
+export default function Bookmarks() {
   return (
-    <View>
-      <Text>HomeScreen2</Text>
+    <View className="bg-white dark:bg-gray-900 flex-1 items-center justify-center">
+      <Text className="text-2xl dark:text-white">Bookmarks</Text>
     </View>
   );
 }
--- app/(tabs)/Profile.tsx
+++ app/(tabs)/Profile.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 export default function Profile() {
   return (
-    <View className='bg-white flex-1'>
-      <Text>Profile</Text>
+    <View className="bg-white dark:bg-gray-900 flex-1 justify-center items-center">
+      <Text className='text-2xl dark:text-white'>Profile</Text>
     </View>
-  )
+  );
 }
